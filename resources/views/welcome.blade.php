4fea646850a1c68d945d34ea280628dc3f9c2dc8
encoder <nahid@encoderit.net>
2024-10-15 12:28:35 +0600

    !

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,15 +92,18 @@
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             // Display the result in the element with id="demo"
-            if (document.getElementById("demo")) {
-                document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+            const element = document.getElementById("demo")
+            if (element) {
+                element.innerHTML = days + "d " + hours + "h " +
                     minutes + "m " + seconds + "s ";
             }
 
             // If the count down is finished, write some text
             if (distance < 0) {
                 clearInterval(x);
-                document.getElementById("demo").innerHTML = "EXPIRED";
+                if (element) {
+                    element.innerHTML = "EXPIRED";
+                }
             }
         }, 1000);
     </script>
