c3e2a00dd58dedab8536dd920bf798a403a0e380
nahid <nahidferdous240@gmail.com>
2024-10-16 17:13:37 +0600

    !

--- app/FirstRoundEvaluation.php
+++ app/FirstRoundEvaluation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FirstRoundEvaluation extends Model
 {
-//    use SoftDeletes;
+    use SoftDeletes;
 
     protected $table = 'first_round_evaluation';
 
