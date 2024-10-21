8dfe6eefd1d4ec6baa9af8e1083cadffc607e6c3
encoder <nahid@encoderit.net>
2024-10-21 17:31:56 +0600

    !

--- resources/views/best-of-10-years-change.blade.php
+++ resources/views/best-of-10-years-change.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
     @if (session()->has('alert-success'))
         <div class="alert alert-success">
@@ -16,131 +17,136 @@
                             @csrf
 
                             <div class="form-group row">
-                                <label for="cat_name" class="col-md-4 col-form-label text-md-right">{{ __('Kategorie') }}</label>
+                                <label for="cat_name"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Kategorie*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="cat_name" type="text" value="{{ $cats->name }}" class="form-control{{ $errors->has('cat_name') ? ' is-invalid' : '' }}" name="cat_name" value="{{ old('cat_name') }}" required readonly>
+                                    <input id="cat_name" type="text" value="{{ $project->cat_name }}"
+                                           class="form-control{{ $errors->has('cat_name') ? ' is-invalid' : '' }}"
+                                           name="cat_name" value="{{ old('cat_name') }}" required readonly>
 
                                     @if ($errors->has('cat_name'))
                                         <span class="invalid-feedback">
-                    <strong>{{ $errors->first('cat_name') }}</strong>
-                </span>
+                                            <strong>{{ $errors->first('cat_name') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
 
-                            {{ Form::hidden('cat_id', $cats->id) }}
-                            {{ Form::hidden('group', $cats->group) }}
 
-
-
-                            @if($user->rolle == 9)
-                                <div class="form-group row">
-                                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Select User') }}</label>
-
-                                    <div class="col-md-6">
-                                        <select name="user_id" id="input" class="form-control" required="required">
-                                            @foreach($users as $single_user)
-                                                <option value="{{$single_user->id}}">{{$single_user->vorname}} {{$single_user->name}} - [{{$single_user->firma}}]</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            @else
-                                <input type="hidden" name="user_id" value="{{$user->id}}">
-                            @endif
+                            {{ Form::hidden('project_id', $project->id) }}
+                            {{ Form::hidden('cat_id', $project->cat_id) }}
 
                             <div class="form-group row">
-                                <label for="beschreibung" class="col-md-4 col-form-label text-md-right">{{ __('Beschreibung*') }}</label>
+                                <label for="projektname"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Projektnamen*') }}</label>
 
                                 <div class="col-md-6">
+                                    <input id="projektname" type="text" value="{{ $project->projektname }}"
+                                           class="form-control{{ $errors->has('projektname') ? ' is-invalid' : '' }}"
+                                           name="projektname" value="{{ old('projektname') }}" readonly>
 
-                                    <textarea id="beschreibung" rows="10" class="form-control{{ $errors->has('beschreibung') ? ' is-invalid' : '' }}" name="beschreibung" value="{{ old('beschreibung') }}" required></textarea>
-                                    Erlaubte Wörter: {{ $cats->words }} - Wörter total : <span id="display_count">0</span> Wörter!
-                                    @if ($errors->has('beschreibung'))
+                                    @if ($errors->has('projektname'))
                                         <span class="invalid-feedback">
-                    <strong>{{ $errors->first('beschreibung') }}</strong>
-                </span>
+                                            <strong>{{ $errors->first('projektname') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
 
-                            <!-- Video -->
+                            <input type="hidden" name="user_id" value="{{ $user->id }}">
+
                             <div class="form-group row">
-                                <label for="youtube" class="col-md-4 col-form-label text-md-right">{{ __('Video*') }}</label>
+                                <label for="beschreibung"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Beschreibung*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="youtube" type="file" class="form-control{{ $errors->has('youtube') ? ' is-invalid' : '' }}" name="youtube" value="{{ old('youtube') }}" accept="video/mp4,video/x-m4v,video/*">
-                                    <input type="hidden" id="uploaded-youtube-file-name" name="uploaded_youtube_file_name">
-                                    @if ($errors->has('youtube'))
+
+                                    <textarea id="beschreibung" rows="10" class="form-control{{ $errors->has('beschreibung') ? ' is-invalid' : '' }}"
+                                              name="beschreibung" value="{{ old('beschreibung') }}" required>{{ $project->beschreibung }}</textarea>
+                                    Erlaubte Wörter: {{ $cats->words }} - Wörter total : <span
+                                            id="display_count_beschreibung">0</span> Wörter!
+                                    @if ($errors->has('$project->beschreibung'))
                                         <span class="invalid-feedback">
-                    <strong>{{ $errors->first('youtube') }}</strong>
-                </span>
+                                            <strong>{{ $errors->first('$project->beschreibung') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="datum" class="col-md-4 col-form-label text-md-right">{{ __('Datum der Hochzeit*') }}</label>
+                                <label for="testimonial"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Referenz mit Kontaktdaten *') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="datum" type="text" class="form-control{{ $errors->has('datum') ? ' is-invalid' : '' }}" name="datum" value="{{ old('datum') }}" required>
-
-                                    @if ($errors->has('datum'))
+                                    <textarea id="testimonial" rows="10" class="form-control{{ $errors->has('testimonial') ? ' is-invalid' : '' }}"
+                                              name="testimonial" value="{{ old('testimonial') }}" required>{{ $project->testimonial }}</textarea>
+                                    Erlaubte Wörter: {{ $cats->words }} - Wörter total : <span
+                                            id="display_count_referenz">0</span> Wörter!
+                                    @if ($errors->has('testimonial'))
                                         <span class="invalid-feedback">
-                    <strong>{{ $errors->first('datum') }}</strong>
-                </span>
+                                            <strong>{{ $errors->first('testimonial') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="ort" class="col-md-4 col-form-label text-md-right">{{ __('Ort der Hochzeit*') }}</label>
+                                <label for="copyright"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Copyright*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="ort" type="text" class="form-control{{ $errors->has('ort') ? ' is-invalid' : '' }}" name="ort" value="{{ old('ort') }}" required>
-
-                                    @if ($errors->has('ort'))
-                                        <span class="invalid-feedback">
-                    <strong>{{ $errors->first('ort') }}</strong>
-                </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="copyright" class="col-md-4 col-form-label text-md-right">{{ __('Copyright*') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="copyright" type="text" class="form-control{{ $errors->has('copyright') ? ' is-invalid' : '' }}" name="copyright" value="{{ old('copyright') }}" required>
+                                    <input id="copyright" type="text" value="{{ $project->copyright }}"
+                                           class="form-control{{ $errors->has('copyright') ? ' is-invalid' : '' }}"
+                                           name="copyright" value="{{ old('copyright') }}" required>
 
                                     @if ($errors->has('copyright'))
                                         <span class="invalid-feedback">
-                    <strong>{{ $errors->first('copyright') }}</strong>
-                </span>
+                                            <strong>{{ $errors->first('copyright') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="check" class="col-md-4 col-form-label text-md-right">{{ __('') }}</label>
+                                <label for="check"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('') }}</label>
 
                                 <div class="col-md-6">
-                                    <input type="checkbox" checked="checked" name="check" id="check" value="{{old('check')}}" required><label for="check"><span></span>
-                                        <p>Ich habe das Recht die Fotos zu verwenden!</p></label>
+                                    <input type="checkbox" checked="checked" name="check" id="check"
+                                           value="{{ old('check') }}" required><label for="check"><span></span>
+                                        <p>Ich habe das Recht die Fotos zu verwenden!</p>
+                                    </label>
                                     @if ($errors->has('check'))
                                         <span class="invalid-feedback">
-                    <strong>{{ $errors->first('check') }}</strong>
-                </span>
+                                            <strong>{{ $errors->first('check') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
 
+
+                            {{-- <div class="form-group row">
+                            <label for="rating"
+                                class="col-md-4 col-form-label text-md-right">{{ __('') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="checkbox" @if ($project->rating_visible) {{ 'checked' }} @endif name="rating_visible" id="rating_visible"
+                                    value="1"><label for="rating_visible"><span></span>
+                                    <p>Sternenbewertung anzeigen</p>
+                                </label>
+                                @if ($errors->has('rating_visible'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('rating_visible') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div> --}}
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <a href="{{ url()->previous() }}" class="btn btn-default">Back</a>
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Projekt anlegen') }}
+                                        {{ __('Projekt ändern') }}
                                     </button>
                                 </div>
                             </div>
@@ -154,8 +160,8 @@
     <script src="https://code.jquery.com/jquery-1.5.js"></script>
     <script>
         var max = {{ $cats->words }};
-        $(document).ready(function () {
-            $("#beschreibung").on('keyup', function () {
+        $(document).ready(function() {
+            $("#beschreibung").on('keyup', function() {
                 var words = this.value.match(/\S+/g).length;
 
                 if (words > max) {
@@ -164,7 +170,7 @@
                     // Add a space at the end to make sure more typing creates new words
                     $(this).val(trimmed + " ");
                 } else {
-                    $('#display_count').text(words);
+                    $('#display_count_beschreibung').text(words);
                     $('#word_left').text(max - words);
                 }
             });
@@ -174,8 +180,8 @@
     <script src="https://code.jquery.com/jquery-1.5.js"></script>
     <script>
         var max = {{ $cats->words }};
-        $(document).ready(function () {
-            $("#testimonial").on('keyup', function () {
+        $(document).ready(function() {
+            $("#testimonial").on('keyup', function() {
                 var words = this.value.match(/\S+/g).length;
 
                 if (words > max) {
@@ -190,4 +196,6 @@
             });
         });
     </script>
+
+    <script src="{{ asset('js/custom.js') }}"></script>
 @endsection
