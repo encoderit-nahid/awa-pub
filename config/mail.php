2af1afe97a3511ee57569b190147916eedea7886
encoder <nahid@encoderit.net>
2024-11-11 12:13:05 +0600

    dropbox access token

--- config/mail.php
+++ config/mail.php
@@ -86,7 +86,7 @@ return [
 
     'username' => env('MAIL_USERNAME'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => 'CgHQu6_nm2' ?? env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
