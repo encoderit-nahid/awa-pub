cbcac5f4e547d50ab7fc133d552063bbdf1fc58e
nahid ferdous <nahidferdous240@gmail.com>
2024-11-12 16:56:26 +0600

    !

--- resources/views/location-outdoor-change.blade.php
+++ resources/views/location-outdoor-change.blade.php
@@ -74,7 +74,7 @@
                                 </div>
                             </div>
 
-                            {{--<div class="form-group row">
+                            <div class="form-group row">
                                 <label for="youtube"
                                        class="col-md-4 col-form-label text-md-right">{{ __('Video*') }}</label>
 
@@ -89,7 +89,7 @@
                                         </span>
                                     @endif
                                 </div>
-                            </div>--}}
+                            </div>
 
                             <div class="form-group row">
                                 <label for="testimonial"
