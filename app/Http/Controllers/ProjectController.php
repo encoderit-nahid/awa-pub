e7ae482dc3a72cd58ab72872b8837c06370612e3
nahid ferdous <nahidferdous240@gmail.com>
2024-12-04 22:03:22 +0600

    !

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1744,12 +1744,10 @@ class ProjectController extends Controller
     public function ProjectFirstRound(Request $request, $cat_id = null)
     {
         $user = Auth::user();
-//    $jury_cats = JuryCategoryPermission::where('user_id', 1157)
-        $jury_cats = JuryCategoryPermission::where('user_id', $user->id)
+    $jury_cats = JuryCategoryPermission::where('user_id', 1157)
+//        $jury_cats = JuryCategoryPermission::where('user_id', $user->id)
             ->pluck('cat_id')
             ->toArray();
-
-        dd($jury_cats);
 
         $all_cats = Cat::orderBy('name')->wherein('id', $jury_cats)->pluck('name', 'id');
 
