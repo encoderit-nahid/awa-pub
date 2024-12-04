b9426053f553e222e8353ccc303b0705eeff362c
encoder <nahid@encoderit.net>
2024-12-04 15:52:28 +0600

    dropbox access token

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1809,9 +1809,9 @@ class ProjectController extends Controller
             if ($cat_id == null) {
                 $projects = Project::join('first_round_evaluation', 'first_round_evaluation.project_id', '=', 'projects.id')
                     ->select('projects.*')
-                    ->where('first_round_evaluation.jury_id', '=', $user->id)
-                    ->whereNull('first_round_evaluation.status')
-                    ->where('stat', '=', '0')
+//                    ->where('first_round_evaluation.jury_id', '=', $user->id)
+//                    ->whereNull('first_round_evaluation.status')
+//                    ->where('stat', '=', '0')
 //                    ->where('is_selected_for_first_evaluation', '=', true)
 //                    ->where('is_failed_first_evolution', '=', false)
                     ->whereIn('cat_id', $jury_cats)
