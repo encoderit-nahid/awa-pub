aada3a7911852e7a99962efc707f945933c10f3d
encoder <nahid@encoderit.net>
2024-12-04 18:31:38 +0600

    dropbox access token

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1749,10 +1749,6 @@ class ProjectController extends Controller
       ->pluck('cat_id')
       ->toArray();
 
-//    $evaluation = FirstRoundEvaluation::where('jury_id', 1157)->where('project_id', 28)
-//      ->get()->toArray();
-//    dd($evaluation);
-
     $all_cats = Cat::orderBy('name')->wherein('id', $jury_cats)->pluck('name', 'id');
 
     $keyword = $request->get('search');
