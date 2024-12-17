9bd9c3f97aa9d849509b880a98491bc9742c48bd
nahid ferdous <nahidferdous240@gmail.com>
2024-12-17 16:44:08 +0600

    Detecting bug

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1668,7 +1668,8 @@ class ProjectController extends Controller
         $all_cats = Cat::orderBy('name')->pluck('name', 'id');
         $pids = Count::WHERE('user_id', $user->id)->pluck('project_id');
 
-        $projects = Project::with('images');
+        $projects = Project::where('jury', '=', '1')
+            ->with('images');
         if ($cat_id != null) {
             $projects = $projects->where('cat_id', $cat_id);
         }
